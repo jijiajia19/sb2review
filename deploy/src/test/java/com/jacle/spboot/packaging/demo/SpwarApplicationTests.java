e1ef84eaac30333b5c43a28a3066af8a4aa69142
jacle <jijiajia19@gmail.com>
2022-01-20 17:35:38 +0800

    war packaging

---
+++ deploy/src/test/java/com/jacle/spboot/packaging/demo/SpwarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacle.spboot.packaging.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpwarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
